5d6316731025572b2dd6c5ffa6740a612cf255ac
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2025-03-15 10:57:15 +0500

    create AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// DATA
+const navbarItems = [
+  { id: 1, name: 'Home', path: '/' },
+  { id: 2, name: 'Favorites', path: '/favorites' },
+]
+</script>
+
+<template>
+  <header class="header">
+    <div class="app-container">
+      <div class="flex-center-between gap-4">
+        <router-link to="/">Movie</router-link>
+        <ul class="nav-list flex-center-between gap-4">
+          <li v-for="item in navbarItems" :key="item.id" class="nav-list__item">
+            <router-link :to="item.path" class="nav-list__item-link">
+              {{ item.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  padding: 12px;
+  background: #1e2324;
+}
+</style>
