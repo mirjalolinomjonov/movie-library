9e7314e455fe3a9e3033fa4764483ee4fba45f9f
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2025-03-15 17:26:52 +0500

    create Moves-list component

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import MovieItem from './MovieItem.vue'
+import type { IMovieItem } from '@/types/index.ts'
+
+interface IMoviesList {
+  category: string
+  itemData: IMovieItem[]
+}
+
+// PROPS
+defineProps<IMoviesList>()
+</script>
+
+<template>
+  <section class="section">
+    <h2 class="section__title">{{ category }}</h2>
+    <div class="grid grid-cols-4 gap-4 my-6">
+      <MovieItem v-for="item in 4" :key="item" />
+    </div>
+
+    <!-- VIEW MORE BUTTON WRAPPER -->
+    <div class="flex-center-center">
+      <button class="view-more-btn">View more</button>
+    </div>
+  </section>
+</template>
