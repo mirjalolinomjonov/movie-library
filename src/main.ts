722c2be6ac9a9afbefbde70021b89d91f43388b0
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2025-03-15 17:23:55 +0500

    setup vue-toastification package

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Toast from 'vue-toastification'
+// Import the CSS or use your own!
+import 'vue-toastification/dist/index.css'
 
+const options = {
+  timeout: 2000,
+}
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(Toast, options)
 
 app.mount('#app')
