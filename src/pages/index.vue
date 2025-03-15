5b8aba5dfbe97fc375d4460eebf0058b8811febe
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2025-03-15 10:57:41 +0500

    create index page

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex-center-center min-h-screen gap-4">
+    <div class="main-btn">cancel</div>
+    <div class="main-btn">save</div>
+  </main>
+</template>
+
+<style scoped lang="scss"></style>
