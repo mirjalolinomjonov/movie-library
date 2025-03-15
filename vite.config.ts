966403356c7430220a14c3a48d71fe4c233caabb
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2025-03-15 12:54:50 +0500

    instal package @vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/styles/_variables.scss";
+        @import "@/assets/styles/_mixins.scss";
+        @import "@/assets/styles/global.scss";
+        `,
+      },
     },
   },
 })
