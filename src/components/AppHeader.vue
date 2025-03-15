7c1fb7ae8a1c2b406221498836c15289f8a1a582
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2025-03-16 00:53:34 +0500

    add search input

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+const store = useStore()
+const route = useRoute()
 // DATA
 const navbarItems = [
   { id: 1, name: 'Home', path: '/' },
@@ -12,6 +16,15 @@ const navbarItems = [
       <div class="flex-center-between gap-4">
         <router-link class="header__logo" to="/">Movie</router-link>
         <ul class="nav-list flex-center-between gap-4">
+          <input
+            v-if="route.name === 'home'"
+            v-model="store.state.searchQuery"
+            class="nav-list__search"
+            placeholder="Search by movie title or genre"
+            type="search"
+            name=""
+            id=""
+          />
           <li v-for="item in navbarItems" :key="item.id" class="nav-list__item">
             <router-link :to="item.path" class="text-white nav-list__item-link">
               {{ item.name }}
@@ -31,6 +44,22 @@ const navbarItems = [
   // header__logo
   &__logo {
     @include font(2rem, 600, 2rem, $white);
+  }
+  .nav-list__search {
+    background: $light-black;
+    width: 300px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    padding: 6px 12px;
+    outline: none;
+    @include font(0.9rem, 500, 1rem, $white);
+    letter-spacing: 1%;
+    &::placeholder {
+      color: #939596;
+    }
+    &:focus {
+      border-color: $white;
+    }
   }
   .nav-list__item-link {
     @include font(1rem, 600, 1rem, $light-black);
